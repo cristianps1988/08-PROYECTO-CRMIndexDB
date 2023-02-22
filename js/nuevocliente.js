b818e8e97f5a795f3108304f50518edafdab10a1
Cristianps1988 <cristianps_1@hotmail.com>
2023-02-22 07:55:31 -0500

    finished

--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -1,5 +1,5 @@
 (function(){
-    let DB;
+    //let DB;
     const formulario = document.querySelector('#formulario');
 
     document.addEventListener('DOMContentLoaded', () => {
@@ -7,51 +7,44 @@
         formulario.addEventListener('submit', validarCliente);
     })
 
-    function conectarDB(){
-        const abrirConexion = window.indexedDB.open('crm', 1);
-        abrirConexion.onerror = () => {
-            console.log('BD conectada con éxito');
-        }
-        abrirConexion.onsuccess = () => {
-            DB = abrirConexion.result;
-        }
-    }
 
     function validarCliente(e){
         e.preventDefault();
 
         // leer los inputs
-        const nombreInput = document.querySelector('#nombre').value;
-        const emailInput = document.querySelector('#email').value;
-        const telefonoInput = document.querySelector('#telefono').value;
-        const empresaInput = document.querySelector('#empresa').value;
+        const nombre = document.querySelector('#nombre').value;
+        const email = document.querySelector('#email').value;
+        const telefono = document.querySelector('#telefono').value;
+        const empresa = document.querySelector('#empresa').value;
 
-        if(nombreInput === '' || emailInput === '' || telefonoInput === '' || empresaInput === ''){
+        if(nombre === '' || email === '' || telefono === '' || empresa === ''){
             imprimirAlerta('Todos los campos son obligatorios', 'error');
             return;
-        }else{
-            console.log('Muy bien');
         }
+        // crear objeto
+        const cliente = {
+            nombre,
+            email,
+            telefono,
+            empresa
+        }
+        cliente.id = Date.now();
+        crearNuevoCliente(cliente);
     }
 
-    function imprimirAlerta(mensaje, tipo){
-        const alerta = document.querySelector('.alerta');
-        if(!alerta){
-            const divMensaje = document.createElement('div');
-            divMensaje.classList.add('px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center', 'border', 'alerta');
-    
-            if(tipo === 'error'){
-                divMensaje.classList.add('bg-red-100', 'border-red-400', 'text-red-700');
-            } else{
-                divMensaje.classList.add('bg-green-100','border-green-400', 'text-green-700' )
-            }
-            divMensaje.textContent = mensaje;
-    
-            formulario.appendChild(divMensaje);
-    
+    function crearNuevoCliente(cliente){
+        const transaction = DB.transaction(['crm'], 'readwrite');
+        const objectStore = transaction.objectStore('crm');
+        objectStore.add(cliente);
+        transaction.onerror = () => {
+            imprimirAlerta('Hubo un error', 'error');
+        };
+        transaction.oncomplete = () => {
+            console.log('Todo en orden');
+            imprimirAlerta('Cliente agregado correctamente');
             setTimeout(() => {
-                divMensaje.remove();
+                window.location.href = 'index.html';
             }, 3000);
-        }
+        };
     }
 })();
